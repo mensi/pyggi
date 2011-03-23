ca16ce728a7d70566af44d6219bfcd0cff41999c
Tobias Heinzen <tobias.heinzen@0xdeadbeef.ch>
2011-03-23 16:28:52 +0100

    simple browser for git repository

--- frontend/views.py
+++ frontend/views.py
@@ -7,3 +7,10 @@ frontend = Module(__name__, 'frontend')
 @templated("frontend/base.xhtml")
 def index():
 	return dict()
+
+@frontend.route("/browse")
+@templated("frontend/browse.xhtml")
+def browser():
+	from lib.repository import GitRepository
+	repo = GitRepository(repo="/home/tobias/git-frontend")
+	return dict(commit=repo.getHead())
---
+++ lib/repository.py
@@ -0,0 +1,19 @@
+from git import Repo
+
+class GitRepository(object):
+	def __init__(self, **kwargs):
+		"""
+			creates a git repository object, that wraps around GitPython to
+			provide a more relaxed acces to features.
+
+			@param kwargs
+				repo=<dir> the directory of the repository to parse
+		"""
+
+		# load up repo
+		self.repo = Repo(kwargs['repo'])
+
+	def getHead(self):
+		return self.repo.commits()[0]
+
+
