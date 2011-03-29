2995614ec3c09ac84b9b7d673b608dbfde1809f8
Tobias Heinzen <tobias.heinzen@0xdeadbeef.ch>
2011-03-29 13:21:53 +0200

    using url_for and endpoints to create better links. removed verbose error messages and redirecting errors to 404 page. renamed module to 'pyggi' and adapted templates

--- frontend/views.py
+++ frontend/views.py
@@ -4,12 +4,23 @@ import os
 
 from lib.decorators import templated, response_mimetype
 from lib.repository import GitRepository
-from flask import Module, current_app, redirect
+from flask import Module, current_app, redirect, url_for
 
-frontend = Module(__name__, 'frontend')
+frontend = Module(__name__, 'pyggi', url_prefix="/pyggi")
 
-@frontend.route("/")
-@templated("frontend/repositories.xhtml")
+def method_shortcut(method='GET'):
+	def ruler(route, endpoint=None, **options):
+		def decorator(f):
+			frontend.add_url_rule(route, endpoint, f, **options)
+			return f
+		return decorator
+	return ruler
+
+get = method_shortcut('GET')
+post = method_shortcut('POST')
+
+@get("/", endpoint='index')
+@templated("pyggi/repositories.xhtml")
 def index():
 	# compute the names of repositories
 	repnames = [GitRepository.getRepositoryFolder(name) \
@@ -23,93 +34,34 @@ def index():
 		]
 	)
 
-@frontend.route("/<repository>/")
+@get("/<repository>/", endpoint='repository')
 def repository(repository):
-	return redirect("/%s/tree/master/" % repository)
-
-@frontend.route("/<repository>/error/repo")
-@templated("frontend/errors/unknown_repo.xhtml")
-def error_unknown_repo(repository):
-	return dict( \
-		repo = repository
-	)
-
-@frontend.route("/<repository>/error/tree")
-@templated("frontend/errors/unknown_tree.xhtml")
-def error_unknown_tree(repository):
-	# check repo
+	# check repository
 	repo_folder = GitRepository.getRepositoryFolder(repository)
 	if repo_folder is None:
-		return redirect('/%s/error/repo' % repository)
+		return redirect(url_for('not_found'))
 	repo = GitRepository(repo=repo_folder)
 
-	# the special tree 'master' will always exist, and thus
-	# we can get a commit object
-	head = repo.getBranchHead("master")
+	return redirect(url_for('browse', repository=repository, tree=repo.repo.active_branch))
 
-	# try to get the tree name
-	try:
-		from flask import request
-		tree = request.values['t']
-	except:
-		tree = ""
+@get("/404", endpoint='not_found')
+@templated("pyggi/404.xhtml")
+def not_found():
+	pass
 
-	return dict( \
-		repo = repository,
-		commit = head,
-		tree = tree
-	)
-
-@frontend.route("/<repository>/error/path")
-@templated("frontend/errors/unknown_path.xhtml")
-def error_unknown_path(repository):
-	# check repo
-	repo_folder = GitRepository.getRepositoryFolder(repository)
-	if repo_folder is None:
-		return redirect('/%s/error/repo' % repository)
-	repo = GitRepository(repo=repo_folder)
-
-	# we will now check if we can get the tree
-	# by checking the contents of the transmitted variable.
-	# if nothing was transmitted, then we obviously got
-	# another error, and we have to handle that
-	try:
-		from flask import request
-		path = request.values['p']
-
-		# if the path is empty, or has not at least
-		# two entries, then we're done also
-		path = path.split("/", 1)
-		if not len(path) == 2:
-			raise KeyError("")
-	except KeyError as error:
-		return redirect('/%s/error/tree?t=' % repository)
-
-	# the first entry in the path will give us the tree
-	# we want to reference
-	head = repo.getBranchHead(path[0])
-	if head is None:
-		return redirect('/%s/error/tree?t=%s' % (repository, path[0]))
-
-	return dict( \
-		repo = repository,
-		commit = head,
-		path = path[1]
-	)
-
-@frontend.route("/<repository>/tree/<tree>/")
-@templated("frontend/browse.xhtml")
+@get("/<repository>/tree/<tree>/", endpoint='browse')
+@templated("pyggi/browse.xhtml")
 def browse(repository, tree):
 	# check if the repository exists
 	repo_folder = GitRepository.getRepositoryFolder(repository)
 	if repo_folder is None:
-		return redirect('/%s/error/repo' % repository)
+		return redirect(url_for('not_found'))
 	repo = GitRepository(repo=repo_folder)
 
 	# check if we can get the branch head
 	head = repo.getBranchHead(tree)
 	if head is None:
-		return redirect('/%s/error/tree?t=%s' % (repository, tree))
+		return redirect(url_for('not_found'))
 
 	return dict( \
 		repo = repository,
@@ -118,19 +70,19 @@ def browse(repository, tree):
 		tree = head.tree.values(),
 	)
 
-@frontend.route("/<repository>/commit/<tree>/")
-@templated("frontend/commit-info.xhtml")
+@get("/<repository>/commit/<tree>/", endpoint='commit')
+@templated("pyggi/commit-info.xhtml")
 def commit(repository, tree):
 	# check if the repository exists
 	repo_folder = GitRepository.getRepositoryFolder(repository)
 	if repo_folder is None:
-		return redirect('/%s/error/repo' % repository)
+		return redirect(url_for('not_found'))
 	repo = GitRepository(repo=repo_folder)
 
 	# check for tree
 	head = repo.getBranchHead(tree)
 	if head is None:
-		return redirect('/%s/error/tree?t=%s' % (repository, tree))
+		return redirect(url_for('not_found'))
 
 	return dict( \
 		repo = repository,
@@ -139,13 +91,13 @@ def commit(repository, tree):
 		diffs = head.diffs
 	)
 
-@frontend.route("/<repository>/tree/<tree>/<path:path>/")
-@templated("frontend/browse.xhtml")
+@get("/<repository>/tree/<tree>/<path:path>/", endpoint='browse_sub')
+@templated("pyggi/browse.xhtml")
 def browse_sub(repository, tree, path):
 	# check if the repository exists
 	repo_folder = GitRepository.getRepositoryFolder(repository)
 	if repo_folder is None:
-		return redirect('/%s/error/repo' % repository)
+		return redirect(url_for('not_found'))
 	repo = GitRepository(repo=repo_folder)
 
 	# the complete path, including the treeish
@@ -154,23 +106,26 @@ def browse_sub(repository, tree, path):
 	# check for tree
 	head = repo.getBranchHead(tree)
 	if head is None:
-		return redirect('/%s/error/tree?t=%s' % (repository, tree))
+		return redirect(url_for('not_found'))
+
+	# save the tree-id
+	treeid = tree
 
 	# get tree by path and check for error
 	tree = repo.getTreeByPath(path)
 	if tree is None:
-		return redirect('/%s/error/path?p=%s' % (repository, path))
+		return redirect(url_for('not_found'))
 
 	return dict( \
 		repo = repository,
-		treeid = tree,
+		treeid = treeid,
 		commit = head,
 		tree = tree.values(),
 		breadcrumbs = path.split("/")[1:]
 	)
 
-@frontend.route("/<repository>/history/<tree>/<path:path>")
-@templated("frontend/history.xhtml")
+@get("/<repository>/history/<tree>/<path:path>", endpoint='history')
+@templated("pyggi/history.xhtml")
 def history(repository, tree, path):
 	# check if the repository exists
 	repo_folder = GitRepository.getRepositoryFolder(repository)
@@ -189,8 +144,8 @@ def history(repository, tree, path):
 		breadcrumbs = path.split("/")[1:]
 	)
 
-@frontend.route("/<repository>/blob/<tree>/<path:path>")
-@templated("frontend/blob.xhtml")
+@get("/<repository>/blob/<tree>/<path:path>", endpoint='blob')
+@templated("pyggi/blob.xhtml")
 def blob(repository, tree, path):
 	# check if the repository exists
 	repo_folder = GitRepository.getRepositoryFolder(repository)
@@ -209,8 +164,8 @@ def blob(repository, tree, path):
 		breadcrumbs = path.split("/")[1:]
 	)
 
-@frontend.route("/<repository>/blame/<tree>/<path:path>")
-@templated("frontend/blame.xhtml")
+@get("/<repository>/blame/<tree>/<path:path>", endpoint='blame')
+@templated("pyggi/blame.xhtml")
 def blame(repository, tree, path):
 	# check if the repository exists
 	repo_folder = GitRepository.getRepositoryFolder(repository)
@@ -230,7 +185,7 @@ def blame(repository, tree, path):
 		breadcrumbs = path.split("/")[1:]
 	)
 
-@frontend.route("/<repository>/raw/<tree>/<path:path>")
+@get("/<repository>/raw/<tree>/<path:path>", endpoint='raw')
 @response_mimetype("text/plain")
 def raw(repository, tree, path):
 	# check if the repository exists
