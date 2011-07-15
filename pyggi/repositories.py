0397566cadaba8c55535da7c834daa7889330ac0
Mark Nevill <mark@nevill.ch>
2011-07-15 18:12:12 +0200

    Added support for repository_base in wsgiorg.routing_args environ variable.

--- pyggi/repositories.py
+++ pyggi/repositories.py
@@ -7,19 +7,26 @@
 
 import os
 import functools
+import logging
 
 from pyggi.lib.decorators import templated, cached
 from pyggi.lib.repository.gitr import GitRepository
-from flask import Blueprint, redirect, url_for
+from flask import Blueprint, redirect, url_for, request
 from pyggi.lib.config import config
 
 frontend = Blueprint('repos', __name__)
 get = functools.partial(frontend.route, methods=['GET'])
 post = functools.partial(frontend.route, methods=['POST'])
 
+def get_repository_base():
+    return request.environ.get('wsgiorg.routing_args', (None, {}))[1].get('repository_base') or config.get('general','git_repositories')
+
+def get_repository_path(name):
+    return os.path.join(get_repository_base(), name)
+
 def cache_keyfn(prefix, additional_fields=[]):
     def test(*args, **kwargs):
-        id = GitRepository.resolve_ref(GitRepository.path(kwargs['repository']), kwargs['tree'])
+        id = GitRepository.resolve_ref(get_repository_path(kwargs['repository']), kwargs['tree'])
 
         path = ""
         for field in additional_fields:
@@ -34,28 +41,32 @@ def cache_keyfn(prefix, additional_fields=[]):
 @templated("repositories.xhtml")
 def index():
     # compute the names of repositories
-    repnames = [name \
-        for name in os.walk(config.get('general','git_repositories')).next()[1] \
-        if GitRepository.isRepository(name)
-    ]
+    try:
+        dirnames = os.walk(get_repository_base()).next()[1]
+    except StopIteration:
+        logging.warning("repository base %s does not exist", dirnames)
+        return dict(repositories=[])
 
-    return dict( \
-        repositories = [ \
-            GitRepository(repository=GitRepository.path(name)) for name in repnames
+    paths = (get_repository_path(name) for name in dirnames)
+
+    paths = (path for path in paths if GitRepository.isRepository(path))
+
+    return dict(
+        repositories = [
+            GitRepository(repository=path) for path in paths
         ]
     )
 
 @get("/<repository>/")
 def repository(repository):
-    repo = GitRepository(repository=GitRepository.path(repository))
+    repo = GitRepository(repository=get_repository_path(repository))
     return redirect(url_for('.overview', repository=repository, tree=repo.active_branch))
-
 
 @get("/<repository>/overview/<tree>/")
 @cached(cache_keyfn('overview'))
 @templated("overview.xhtml")
 def overview(repository, tree):
-    repo = GitRepository(repository=GitRepository.path(repository))
+    repo = GitRepository(repository=get_repository_path(repository))
 
     return dict( \
         repository = repo,
@@ -66,7 +77,7 @@ def overview(repository, tree):
 @cached(cache_keyfn('browse'))
 @templated("browse.xhtml")
 def browse(repository, tree):
-    repo = GitRepository(repository=GitRepository.path(repository))
+    repo = GitRepository(repository=get_repository_path(repository))
 
     return dict( \
         repository = repo,
@@ -78,7 +89,7 @@ def browse(repository, tree):
 @cached(cache_keyfn('tree', ['path']))
 @templated("browse.xhtml")
 def browse_sub(repository, tree, path):
-    repo = GitRepository(repository=GitRepository.path(repository))
+    repo = GitRepository(repository=get_repository_path(repository))
 
     return dict( \
         repository = repo,
@@ -91,7 +102,7 @@ def browse_sub(repository, tree, path):
 @cached(cache_keyfn('commit'))
 @templated("commit.xhtml")
 def commit(repository, tree):
-    repo = GitRepository(repository=GitRepository.path(repository))
+    repo = GitRepository(repository=get_repository_path(repository))
 
     return dict( \
         repository = repo,
@@ -102,7 +113,7 @@ def commit(repository, tree):
 @cached(cache_keyfn('blob', ['path']))
 @templated("blob.xhtml")
 def blob(repository, tree, path):
-    repo = GitRepository(repository=GitRepository.path(repository))
+    repo = GitRepository(repository=get_repository_path(repository))
 
     return dict( \
         repository = repo,
@@ -114,7 +125,7 @@ def blob(repository, tree, path):
 @cached(cache_keyfn('blame', ['path']))
 @templated("blame.xhtml")
 def blame(repository, tree, path):
-    repo = GitRepository(repository=GitRepository.path(repository))
+    repo = GitRepository(repository=get_repository_path(repository))
 
     return dict( \
         repository = repo,
@@ -125,7 +136,7 @@ def blame(repository, tree, path):
 @get("/<repository>/raw/<tree>/<path:path>")
 @cached(cache_keyfn('raw', ['path']))
 def raw(repository, tree, path):
-    repo = GitRepository(repository=GitRepository.path(repository))
+    repo = GitRepository(repository=get_repository_path(repository))
     blob = repo.blob('/'.join([tree,path]))
 
     # create a response with the correct mime type
@@ -137,7 +148,7 @@ def raw(repository, tree, path):
 @get("/<repository>/download/<tree>")
 @cached(cache_keyfn('download'))
 def download(repository, tree):
-    repo = GitRepository(repository=GitRepository.path(repository))
+    repo = GitRepository(repository=get_repository_path(repository))
     data = repo.archive(tree)
 
     # create a response with the correct mime type
