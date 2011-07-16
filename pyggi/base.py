4a718e0361d328076b1ceb22d1f4a11e7a2caf12
Mark Nevill <mark@nevill.ch>
2011-07-16 13:43:42 +0200

    Made styling static css.

--- pyggi/base.py
+++ pyggi/base.py
@@ -13,20 +13,6 @@ post = functools.partial(base.route, methods=['POST'])
 def favicon():
     return redirect(pyggi.lib.filters.static_url_for("favicon.ico"))
 
-@get("/style")
-def style():
-    from flask import render_template
-    from flask import make_response
-
-    data = render_template("style/common.css")
-    data = data.replace("\n", "")
-    data = data.replace("\t", "")
-
-    response = make_response(data)
-    response.mimetype = 'text/css'
-
-    return response
-
 @get("/404")
 @templated("404.xhtml")
 def not_found():
